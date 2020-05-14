6735a407b89b8b7a39f039630ec021cbda6fee3c
unknown <youssef.m5557@gmail.com>
2020-05-14 11:03:32 +0200

    Add new question in exam action is implemented

--- newproject/Software2 project/Controllers/ProfessorController.cs
+++ newproject/Software2 project/Controllers/ProfessorController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Software2_project.Context;
+using Software2_project.Models;
+using Software2_project.ViewModel;
 
 namespace Software2_project.Controllers
 {
@@ -26,6 +29,54 @@ namespace Software2_project.Controllers
             }
 
             return RedirectToAction("Login", "Home");
+        }
+
+        public ActionResult viewCourses()
+        {
+            if (Session["username"] != null && Session["role"].Equals("professor"))
+            {
+                short id = (short)Session["id"];
+                ProfessorModel professor = _context.professorDb.Where(p => p.id == id).Single();
+                var CoursesOfProfessor = professor.courseModel.ToList();
+                return View(CoursesOfProfessor);
+            }
+
+            return RedirectToAction("Login", "Home");
+        }
+
+        public ActionResult addQuestion(short id)
+        {
+            if (Session["username"] != null && Session["role"].Equals("professor"))
+            {
+                CourseModel course = _context.courseDb.Single(c => c.id == id);
+
+                var viewModel = new QuestionCourseViewModel
+                {
+                    course = course,
+                    question = new QuestionModel { CourseId = course.id }
+                };
+
+                return View("addQuestion", viewModel);
+            }
+
+            return RedirectToAction("Login", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult CreateQuestion(QuestionModel Question, string saveE, string newQ)
+        {
+            if (Question.id == 0)
+                _context.questionDb.Add(Question);
+
+            _context.SaveChanges();
+
+            if (saveE != null)
+                return RedirectToAction("loggedIn", "Professor");
+
+            else if (newQ != null)
+                return RedirectToAction("addQuestion", new RouteValueDictionary(new { Controller = "Professor", Action = "addQuestion", id = Question.CourseId }));
+
+            else return null;
         }
     }
 }
